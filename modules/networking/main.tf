8f527f6be65cbafa2cac6df83f9d11b687cc3241
Renzo DF <renzo.di.fede@gmail.com>
2021-10-29 02:20:48 -0300

    x

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -10,12 +10,8 @@ module "vpc" {
   name = "${var.namespace}-vpc"
   cidr = "10.0.0.0/16"
   azs = data.aws_availability_zones.available.names
-  private_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"]
   public_subnets = [
-    "10.0.101.0/24",
-    "10.0.102.0/24"]
+    "10.0.101.0/24"]
   tags = {
     name = "${var.namespace}-vpc"
   }

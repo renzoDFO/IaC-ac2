eae413e4ddc2b1059367c0fcde3c40c970296ce3
Renzo DF <renzo.di.fede@gmail.com>
2021-11-02 18:13:34 -0300

    ssh dedsde internet

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "${var.namespace}-vpc"
   cidr = "10.0.0.0/16"
-  azs = ["us-east-1"]
+  azs = ["us-east-1a"]
   public_subnets = [
     "10.0.101.0/24"]
   private_subnets = [

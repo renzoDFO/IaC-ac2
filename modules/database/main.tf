d92a82dfeb44ce307cd6f2bcce57abeb43c45e7e
Renzo DF <renzo.di.fede@gmail.com>
2021-11-02 18:11:21 -0300

    ssh dedsde internet

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "database" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = [
-      "10.0.0.0/16"]
+      "0.0.0.0/0"]
   }
   ingress {
     description = "27017 desde mi backend"
--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "frontend" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = [
-      "10.0.0.0/16"]
+      "0.0.0.0/0"]
   }
   ingress {
     description = "80 desde internet"
